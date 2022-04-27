c439dbebb35193591a626cd189f9a5277d22c00e
annissa-tessffaaye <annissatessffaaye@gmail.com>
2022-04-27 09:38:37 +0100

    AT: Edit User.java and correcting spelling mistakes

--- src/main/java/com/qa/baeprojectone/BaeprojectoneApplication.java
+++ src/main/java/com/qa/baeprojectone/BaeprojectoneApplication.java
@@ -1,4 +1,4 @@
-package com.qa.naeprojectone;
+package com.qa.baeprojectone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
