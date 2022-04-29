f43005264d37290d8445c3683911aa0a42961641
annissa-tessffaaye <annissatessffaaye@gmail.com>
2022-04-29 10:06:29 +0100

    AT: Added Swagger UI dependency was in pom.xml file and created UserControllerIngtrationTest.java and UserControllerUnitTest.java. Created an index.html web page for CRUD operations.

--- src/test/java/com/qa/baeprojectone/Controller/UserControllerUnitTest.java
+++ src/test/java/com/qa/baeprojectone/Controller/UserControllerUnitTest.java
@@ -1,1 +1,108 @@
 package com.qa.baeprojectone.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.baeprojectone.domain.User;
+import com.qa.baeprojectone.service.UserService;
+
+@WebMvcTest
+public class UserControllerUnitTest {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  @MockBean
+  private UserService service;
+
+  @Test
+  void createTest() throws Exception {
+      User entry = new User("Tom", "Jones", "tjones1", "12346", "123st");
+      String entryAsJSON = this.mapper.writeValueAsString(entry);
+
+      Mockito.when(this.service.create(entry)).thenReturn(entry);
+
+      mvc.perform(post("/user/create")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(entryAsJSON))
+              .andExpect(status().isCreated())
+              .andExpect(content().json(entryAsJSON));
+  }
+
+  @Test
+  public void getAllTest() throws Exception {
+      User entry = new User("Tom", "Jones", "tjones1", "12346", "123st");
+      List<User> output = new ArrayList<>();
+      output.add(entry);
+      String outputAsJSON = this.mapper.writeValueAsString(output);
+
+      Mockito.when(this.service.getAll()).thenReturn(output);
+
+      mvc.perform(get("/user/getAll")
+              .contentType(MediaType.APPLICATION_JSON))
+              .andExpect(status().isOk())
+              .andExpect(content().json(outputAsJSON));
+  }
+
+  @Test
+  public void getByIdTest() throws Exception {
+      User entry = new User("Tom", "Jones", "tjones1", "12346", "123st");
+      String entryAsJSON = this.mapper.writeValueAsString(entry);
+
+      Mockito.when(this.service.getById(1L)).thenReturn(entry);
+
+      mvc.perform(get("/user/getById/1")
+              .contentType(MediaType.APPLICATION_JSON))
+              .andExpect(status().isOk())
+              .andExpect(content().json(entryAsJSON));
+  }
+
+  @Test
+  public void updateTest() throws Exception {
+      User entry = new User("Tom", "Jones", "tjones1", "12346", "123st");
+      String entryAsJSON = this.mapper.writeValueAsString(entry);
+
+      Mockito.when(this.service.update(1L, entry)).thenReturn(entry);
+
+      mvc.perform(put("/user/update/1")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(entryAsJSON))
+              .andExpect(status().isAccepted())
+              .andExpect(content().json(entryAsJSON));
+  }
+
+  @Test
+  public void deleteTest() throws Exception {
+      Mockito.when(this.service.delete(1L)).thenReturn(true);
+
+      mvc.perform(delete("/user/delete/1")
+              .contentType(MediaType.APPLICATION_JSON))
+              .andExpect(status().isNoContent());
+  }
+
+  @Test
+  public void deleteFailTest() throws Exception {
+      Mockito.when(this.service.delete(2L)).thenReturn(false);
+
+      mvc.perform(delete("/user/delete/2")
+              .contentType(MediaType.APPLICATION_JSON))
+              .andExpect(status().isInternalServerError());
+  }
+}
